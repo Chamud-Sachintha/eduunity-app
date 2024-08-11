4bbc8b445e03717816affe91cbe3404bde86414d
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-11 16:15:42 +0530

    add changes

--- src/app/modules/web-module/leaning/leaning.component.ts
+++ src/app/modules/web-module/leaning/leaning.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonicModule, IonModal } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 
 @Component({
   selector: 'app-leaning',
@@ -10,7 +11,27 @@ import { IonicModule } from '@ionic/angular';
 })
 export class LeaningComponent  implements OnInit {
 
+  @ViewChild(IonModal) modal!: IonModal;
+
+  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+  name!: string;
+
   constructor() { }
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(this.name, 'confirm');
+  }
+
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      this.message = `Hello, ${ev.detail.data}!`;
+    }
+  }
 
   ngOnInit() {}
 
